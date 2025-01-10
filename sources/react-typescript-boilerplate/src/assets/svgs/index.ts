4aff477b2ee18816133cd4aa20930601f8958e6d
haorocks <hao@avianjets.com>
2025-01-10 11:02:44 +0700

    feat: remove source

--- sources/react-typescript-boilerplate/src/assets/svgs/index.ts
+++
@@ -1,6 +0,0 @@
-import { ReactComponent as IconXCircle } from "./x-circle-outline.svg";
-import { ReactComponent as IconX } from "./x.svg";
-import { ReactComponent as IconCheckCircleOutline } from "./check-circle-outline.svg";
-import { ReactComponent as IconSpinner } from "./spinner.svg";
-
-export { IconXCircle, IconX, IconCheckCircleOutline, IconSpinner };
--- sources/react-typescript-boilerplate/src/store/auth/reducer.ts
+++
@@ -1,9 +0,0 @@
-import { createReducer } from "@reduxjs/toolkit";
-
-const initialState = {
-	user: null
-};
-
-export const authReducer = createReducer(initialState, (builder) => {
-	builder.addCase("fulfilled", (state, action) => {});
-});
